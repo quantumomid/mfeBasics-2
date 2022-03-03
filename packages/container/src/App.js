d543d739c24953c6aac61dbcd77d7a96694ee060
quantumomid <omid.wakili@outlook.com>
2022-03-03 13:21:51 +0000

    do initial set up for Container app - webpack and react files

---
+++ packages/container/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <h1>This is the CONTAINER for the microfrontend apps</h1>
+    )
+}
+
+export default App;
---
+++ packages/container/src/index.js
@@ -0,0 +1,2 @@
+// To allow webpack the time to load all the dependencies needed to run bootstrap.js file
+import("./bootstrap");
