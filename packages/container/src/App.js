913a7ee4f49c976f15ad1c43e9e5ba3e11886b29
quantumomid <omid.wakili@outlook.com>
2022-03-03 16:22:39 +0000

    complete integration of Marketing and Container and configure shared modules such as react and react-dom between two apps in MF plugin

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { mount } from "marketing/MarketingApp";
+import MarketingApp from "./components/MarketingApp";
 
 console.log(mount);
 
 const App = () => {
     return (
-        <h1>This is the CONTAINER for the microfrontend apps</h1>
+        <Fragment>
+            <h1>This is the CONTAINER for the microfrontend apps</h1>
+            <hr />
+            <MarketingApp />
+        </Fragment>
     )
 }
 
