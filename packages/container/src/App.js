209d01da577c775d1dc40edd8f171f6c6975d48d
quantumomid <omid.wakili@outlook.com>
2022-03-10 08:25:00 +0000

    add production string to css class names to overcome clash issue caused between Landing from Marketing and Header from Container in production site

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,14 +3,21 @@ import { mount } from "marketing/MarketingApp";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
-console.log(mount);
+// console.log(mount);
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: "co",
+});
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Header />
-            <MarketingApp />
+            <StylesProvider generateClassName={generateClassName}>
+                <Header />
+                <MarketingApp />
+            </StylesProvider>
         </BrowserRouter>
     )
 }
