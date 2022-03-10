0428a77a4a52cbbfbc037b81bc7d4f87d3ad8fd9
quantumomid <omid.wakili@outlook.com>
2022-03-10 07:33:48 +0000

    add header component to container app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,17 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { mount } from "marketing/MarketingApp";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
 
 console.log(mount);
 
 const App = () => {
     return (
-        <Fragment>
-            <h1>This is the CONTAINER for the microfrontend apps</h1>
-            <hr />
+        <BrowserRouter>
+            <Header />
             <MarketingApp />
-        </Fragment>
+        </BrowserRouter>
     )
 }
 
