5a563e6f6d6c6c812241e2081a6d49940659e6dc
quantumomid <omid.wakili@outlook.com>
2022-03-11 07:02:16 +0000

    fix GET error caused by publicPath mismatch issue - also fix in container and marketing for now to prevent any errors in future

--- packages/auth/src/components/Signin.js
+++ packages/auth/src/components/Signin.js
@@ -17,7 +17,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
       <Link color="inherit" to="/">
-        Your Website
+        ☁ Al-Cloud
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
